cb69ee6e8c6270a4e6be95d9236d1eff9b50c3d7
lee <dominik@axodo.ch>
2025-10-31 23:41:26 +0100

    create basic server logic for getting files

--- code/frontend/server.js
+++ code/frontend/server.js
@@ -1,12 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('running');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`Server läuft auf http://localhost:${PORT}`);
