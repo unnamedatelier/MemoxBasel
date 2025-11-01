98184ac3f4b62cb85c0118e81b820e50a0bad05a
Albinoson <salomon.ibrahim1234@gmail.com>
2025-11-01 01:28:15 +0100

    button function

--- code/frontend/public/script.js
+++ code/frontend/public/script.js
@@ -1,10 +1,10 @@
 function joinConference() {
     // Navigate to Gruppe1/index.html
     console.log("window.location.href = 'conferences/Gruppe1/index.html';")
-    window.location.href = 'conferences/Gruppe1/index.html';
+    window.location.href = 'conferences/Gruppe1/';
 }
 
 function createConference() {
     // Navigate to Gruppe1/admin/index.html
-    window.location.href = 'conferences/Gruppe1/admin/index.html';
+    window.location.href = 'conferences/Gruppe1/admin/';
 }
